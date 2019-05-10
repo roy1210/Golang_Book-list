7c45cb739a066cd630b718f353755e342eb76602
roy1210 <rswkure@gmail.com>
2019-05-11 07:19:04 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
+}
+
+var books []Book
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	log.Println("Gets all books")
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Gets one books")
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Adds one book")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Updates a book")
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Removes a book")
+}
