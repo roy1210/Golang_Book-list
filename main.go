a1d35704d62511ad5754963339d5c2b6b0f069a1
roy1210 <rswkure@gmail.com>
2019-05-12 08:01:52 +0800

    set DB function

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/subosito/gotenv"
 )
 
+// Book struct
 type Book struct {
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
@@ -36,17 +37,17 @@ func logFatal(err error) {
 
 func main() {
 
-	pgUrl, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	pgURL, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
 	logFatal(err)
 
-	db, err = sql.Open("postgres", pgUrl)
+	db, err = sql.Open("postgres", pgURL)
 	logFatal(err)
 
 	err = db.Ping()
 	logFatal(err)
 
 	// ElephantSQL: -`dbname`, `host`, `password`, `port`, `user`
-	log.Println(pgUrl)
+	log.Println(pgURL)
 
 	// PUT: replace resource information
 	router := mux.NewRouter()
@@ -78,20 +79,60 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	params := mux.Vars(r)
 
+	rows := db.QueryRow("select * from books where id=$1", params["id"])
+
+	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(book)
 }
 
 // r: request => such come from Client or Postman
+// Book.ID made in this struct is not syncing with the serialized ID in the elephantSQL. Will replace by the ID made by elephantSLQ as serialized.
 func addBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	var bookID int
+	// Decode: Map to the arg
+	json.NewDecoder(r.Body).Decode(&book)
 
+	// log.Println(book) >> 2019/05/12 05:00:48 {19 C++ is old Mr. C++ 2014}
+	// log.Println(book)
+	// log.Println(reflect.TypeOf(book))
+	// >> main.Book => Means struct
+
+	// Scan: likes copy the row to the bookID row
+	err := db.QueryRow("insert into books (title, author, year) values ($1, $2, $3) RETURNING id;", book.Title, book.Author, book.Year).Scan(&bookID)
+	logFatal(err)
+
+	// ex >> 4    NOT LIKES `ID:99` ALLOCATED BY MY SELF
+	json.NewEncoder(w).Encode(bookID)
+	log.Println(book)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
 
+	// where : which data is going to update
+	result, err := db.Exec("update books set title = $1, author=$2, year=$3 where id=$4 RETURNING id", &book.Title, &book.Author, &book.Year, &book.ID)
+
+	rowsUpdated, err := result.RowsAffected()
+	logFatal(err)
+	json.NewEncoder(w).Encode(rowsUpdated)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	result, err := db.Exec("delete from books where id = $1", params["id"])
+	logFatal(err)
 
+	rowsDeleted, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsDeleted)
 }
 
 /*
